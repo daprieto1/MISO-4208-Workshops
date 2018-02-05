7008884e501fecf08708031cfaee39dacb792532
daprieto1 <da.prieto1@uniandes.edu.co>
2018-02-05 17:42:06 -0500

    add firebase

--- workshop-1/ratp-pwa/scripts/app.js
+++ workshop-1/ratp-pwa/scripts/app.js
@@ -243,9 +243,9 @@
         };
     }
 
-    /*if ('serviceWorker' in navigator) {
+    if ('serviceWorker' in navigator) {
         navigator.serviceWorker
             .register('./service-worker.js')
             .then(function () { console.log('Service Worker Registered'); });
-    }*/
+    }
 })();
