2ad278b96109150307d37cf56fb786bc3f0ab571
System Administrator <root@dev-angelaa.visionsoftware.loc>
2018-02-09 18:43:18 -0500

    finish protractor workshop

--- workshop-2/protractor/tour-of-heroes-cli/e2e/app.e2e-spec.ts
+++ workshop-2/protractor/tour-of-heroes-cli/e2e/app.e2e-spec.ts
@@ -35,7 +35,7 @@ describe('Tour of heroes, heroes page', () => {
 
 });
 
-describe('Tour of heroes, search for hero', function () {
+describe('Tour of heroes, search for hero', () => {
   let page: TourOfHeroesPage;
 
   beforeEach(() => {
@@ -43,9 +43,84 @@ describe('Tour of heroes, search for hero', function () {
     page.navigateTo();
   });
 
-  it('should find specific hero page', function () {
+  it('should find Narco hero page', () => {
     var result = page.searchForHero('Narco');
     expect(result.count()).toBe(1);
+    expect(result.first().getText()).toEqual('Narco details!');
+  });
+
+  it('should find Bombasto hero page', () => {
+    var result = page.searchForHero('Bombasto');
+    expect(result.count()).toBe(1);
+    expect(result.first().getText()).toEqual('Bombasto details!');
+  });
+});
+
+describe('Tour of heroes, delete hero', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+  });
+
+  it('should navigate to heroes', () => {
+    page.navigateToHeroes();
+    expect(page.getAllHeroes().count()).toBe(11);
+  });
+
+  it('should find delete', () => {
+    page.deleteHero();
+    expect(page.getAllHeroes().count()).toBe(10);
+  })
+
+});
+
+describe('Tour of heroes, edit hero', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage;
+  });
+
+  it('should display top 4 heroes original', () => {
+    page.navigateTo();
+    expect(page.getTop4Heroes()).toEqual(['Mr. Nice', 'Narco', 'Bombasto', 'Celeritas']);
+  });
+
+  it('should find specific hero page', () => {
+    var result = page.searchForHero('Mr. Nice');
+    expect(result.count()).toBe(1);
+    expect(result.first().getText()).toEqual('Mr. Nice details!');
+  });
+
+  it('should display top 4 heroes but with edited content', () => {
+    page.editHero('Mr. Tests');
+    expect(page.getTop4Heroes()).toEqual(['Mr. Tests', 'Narco', 'Bombasto', 'Celeritas']);
+  });
+});
+
+describe('Tour of heroes, Navigate', () => {
+  let page: TourOfHeroesPage;
+
+  beforeEach(() => {
+    page = new TourOfHeroesPage();
+  });
+
+  it('navigate to hero from dashboard', () => {
+    page.navigateTo();
+    var result = page.navigateToHero('dashboard');
+    expect(result.first().getText()).toContain('details!');
+  });
+
+  it('navigate to hero from hero list', () => {
+    page.navigateToHeroes();
+    var result = page.navigateToHero('heroList');
+    expect(result.first().getText()).toContain('details!');
+  });
+
+  it('navigate to hero from searching', () => {
+    page.navigateTo();
+    var result = page.searchForHero('Narco');
     expect(result.first().getText()).toEqual('Narco details!');
   });
 });
--- workshop-2/protractor/tour-of-heroes-cli/e2e/app.po.ts
+++ workshop-2/protractor/tour-of-heroes-cli/e2e/app.po.ts
@@ -27,4 +27,40 @@ export class TourOfHeroesPage {
     element(by.css('.search-result')).click();
     return element.all((by.tagName('h2')));
   }
+
+  deleteHero() {
+    element(by.tagName('my-heroes'))
+      .all(by.tagName('li'))
+      .first()
+      .element(by.tagName('button'))
+      .click();
+  }
+
+  editHero(newName: string) {
+    var input = element(by.tagName("input"));
+    input.clear();
+    input.sendKeys(newName);
+    element(by.buttonText('Save')).click();
+    browser.waitForAngular();
+  }
+
+  navigateToHero(wayToNavigate: string) {
+    switch (wayToNavigate) {
+      case 'dashboard':
+        element.all(by.css('.module.hero'))
+          .all(by.tagName('h4'))
+          .first()
+          .click();
+        break;
+      case 'heroList':
+        element(by.tagName('my-heroes'))
+          .all(by.tagName('li'))
+          .first()
+          .click();
+        element(by.buttonText('View Details')).click();
+        break;
+    }
+    browser.waitForAngular();
+    return element.all((by.tagName('h2')));
+  }
 }
